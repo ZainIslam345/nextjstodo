472a6099998e14fb90b53d2fa9b34ed29a652b7b
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 20:34:40 +0500

    done

--- app/hocs/withAuth.jsx
+++ app/hocs/withAuth.jsx
@@ -20,23 +20,20 @@
 
 // export default WithAuth;
 
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const WithAuth = (WrappedComponent) => {
-
-  
-
   return (props) => {
     const router = useRouter();
-    
+
     useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem('user'));
-        const token = userData ? userData.token : null;
-        console.log(token,"token")
-        if(!token) {
-            router.push('/login');
-        }
+      const userData = JSON.parse(localStorage.getItem("user"));
+      const token = userData ? userData.token : null;
+      console.log(token, "token");
+      if (!token) {
+        router.push("/login");
+      }
     }, []);
 
     return <WrappedComponent {...props} />;
