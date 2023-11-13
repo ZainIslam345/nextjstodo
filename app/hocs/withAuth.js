d4706985e1bf855f1bcfb1c09c65688262b23818
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 18:15:28 +0500

    fixs issues

--- app/hocs/withAuth.js
+++ app/hocs/withAuth.js
@@ -1,23 +1,52 @@
-import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const WithAuth = (WrappedComponent) => {
-  const router = useRouter();
+  const Wrapper = (props) => {
+    const router = useRouter();
 
-  useEffect(() => {
+    useEffect(() => {
       const userData = JSON.parse(localStorage.getItem('user'));
       const token = userData ? userData.token : null;
-      console.log(token,"token")
-      if(!token) {
-          router.push('/login');
+      console.log(token, "token");
+      if (!token) {
+        router.push('/login');
       }
-  }, []);
+    }, [router]);
 
-  return (props) => {
     return <WrappedComponent {...props} />;
   };
+
+  Wrapper.displayName = 'WithAuth';
+
+  return Wrapper;
 };
 
-WithAuth.displayName = 'WithAuth';
-
 export default WithAuth;
+
+
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+
+// const WithAuth = (WrappedComponent) => {
+
+//   const router = useRouter();
+//   useEffect(() => {
+//       const userData = JSON.parse(localStorage.getItem('user'));
+//       const token = userData ? userData.token : null;
+//       console.log(token,"token")
+//       if(!token) {
+//           router.push('/login');
+//       }
+//   }, []);
+
+//   return (props) => {
+
+
+//     return <WrappedComponent {...props} />;
+//   };
+// };
+
+// WithAuth.displayName = 'WithAuth';
+
+// export default WithAuth;
