b264491b6e81cda7d0b4f472561fe8521dc7b292
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 20:33:35 +0500

    done

--- app/hocs/withAuth.jsx
+++ app/hocs/withAuth.jsx
@@ -1,46 +1,46 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-const WithAuth = (WrappedComponent) => {
-  const Wrapper = (props) => {
-    const router = useRouter();
-
-    const userData = JSON.parse(localStorage.getItem("user"));
-    const token = userData ? userData.token : null;
-    console.log(token, "token");
-    if (!token) {
-      router.push("/login");
-    }
-
-    return <WrappedComponent {...props} />;
-  };
-
-  return Wrapper;
-};
-
-export default WithAuth;
-
-// import { useRouter } from 'next/router';
-// import { useEffect } from 'react';
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
 // const WithAuth = (WrappedComponent) => {
+//   const Wrapper = (props) => {
+//     const router = useRouter();
 
-//   const router = useRouter();
-//   useEffect(() => {
-//       const userData = JSON.parse(localStorage.getItem('user'));
-//       const token = userData ? userData.token : null;
-//       console.log(token,"token")
-//       if(!token) {
-//           router.push('/login');
-//       }
-//   }, []);
-
-//   return (props) => {
+//     const userData = JSON.parse(localStorage.getItem("user"));
+//     const token = userData ? userData.token : null;
+//     console.log(token, "token");
+//     if (!token) {
+//       router.push("/login");
+//     }
 
 //     return <WrappedComponent {...props} />;
 //   };
+
+//   return Wrapper;
 // };
 
-// WithAuth.displayName = 'WithAuth';
-
 // export default WithAuth;
+
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const WithAuth = (WrappedComponent) => {
+
+  
+
+  return (props) => {
+    const router = useRouter();
+    
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem('user'));
+        const token = userData ? userData.token : null;
+        console.log(token,"token")
+        if(!token) {
+            router.push('/login');
+        }
+    }, []);
+
+    return <WrappedComponent {...props} />;
+  };
+};
+
+export default WithAuth;
