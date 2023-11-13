9532728d92a2da7ec4a3a86721e169e517f16fc0
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 18:09:42 +0500

    done with it

--- app/hocs/withAuth.js
+++ app/hocs/withAuth.js
@@ -2,18 +2,18 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const WithAuth = (WrappedComponent) => {
+  const router = useRouter();
+
+  useEffect(() => {
+      const userData = JSON.parse(localStorage.getItem('user'));
+      const token = userData ? userData.token : null;
+      console.log(token,"token")
+      if(!token) {
+          router.push('/login');
+      }
+  }, []);
+
   return (props) => {
-    const router = useRouter();
-
-    useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem('user'));
-        const token = userData ? userData.token : null;
-        console.log(token,"token")
-        if(!token) {
-            router.push('/login');
-        }
-    }, []);
-
     return <WrappedComponent {...props} />;
   };
 };
