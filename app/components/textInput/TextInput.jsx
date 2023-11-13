8e6c229d0514f7c900aab64726a6b11a61faf6fd
ZainIslam345 <zainislam345@gmail.com>
2023-11-13 17:38:43 +0500

    deploy bug fixing

--- app/components/textInput/TextInput.jsx
+++ app/components/textInput/TextInput.jsx
@@ -2,41 +2,35 @@ import React, { forwardRef } from "react";
 import styles from "./styles.module.css";
 import { Field, ErrorMessage } from "formik";
 
-const TextInput = forwardRef(
-  (
-    {
-      id,
-      label,
-      name,
-      touched,
-      containerClass = "",
-      error,
-      classChange = false,
-      type = "text",
-      ...rest
-    },
-    ref
-  ) => {
-    return (
-      <div
-        className={`${
-          classChange ? "" : ` ${styles.textInput}`
-        } ${containerClass} `}
-      >
-        {label && <label htmlFor={id}>{label}</label>}
-        <Field
-          name={name}
-          ref={ref}
-          type={type}
-          id={id}
-          className={error && touched[id] ? `${styles.textInput_error}` : null}
-          {...rest}
-        />
-        <ErrorMessage name={name} component="span" className={styles.erro} />
-      </div>
-    );
-  }
-);
+const TextInput = ({
+  id,
+  label,
+  name,
+  touched,
+  containerClass = "",
+  error,
+  classChange = false,
+  type = "text",
+  ...rest
+}) => {
+  return (
+    <div
+      className={`${
+        classChange ? "" : ` ${styles.textInput}`
+      } ${containerClass} `}
+    >
+      {label && <label htmlFor={id}>{label}</label>}
+      <Field
+        name={name}
+        type={type}
+        id={id}
+        className={error && touched[id] ? `${styles.textInput_error}` : null}
+        {...rest}
+      />
+      <ErrorMessage name={name} component="span" className={styles.erro} />
+    </div>
+  );
+};
 
 TextInput.displayName = "TextInput";
 
